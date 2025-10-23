1463337d5cffb9c642d752e294e7724f60eda050
Sima Tolstikova <simatolstikova@gmail.com>
2025-10-23 09:44:53 +0300

    fix: use psycopg3 instead of psycopg2 for python 3.13 compatibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-import psycopg2
+import psycopg
 import os
 from urllib.parse import urlparse
 
@@ -8,14 +8,7 @@ app = Flask(__name__)
 # Подключение к БД
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
-    url = urlparse(DATABASE_URL)
-    conn = psycopg2.connect(
-        database=url.path[1:],
-        user=url.username,
-        password=url.password,
-        host=url.hostname,
-        port=url.port
-    )
+    conn = psycopg.connect(DATABASE_URL)
 else:
     conn = None
 
